a2cc268ac4f3b68b7ddec8fade264d60eabf9919
bilozir-info <96798351+bilozir-info@users.noreply.github.com>
2022-01-02 21:23:41 +0200

    Delete assets/js/framework directory

--- assets/js/framework/tools/util.js
+++
@@ -1,9 +0,0 @@
-export const wfm = {
-    delay(ms = 1000) {
-        return new Promise( (resolve, reject) => {
-            setTimeout(() => {
-                resolve()
-            }, ms);
-        });
-    }
-};
