05693569027e93e6a032f623b2ec9f1d1ea8a5f6
bilozir-info <96798351+bilozir-info@users.noreply.github.com>
2022-01-02 21:24:53 +0200

    Delete publications.js

--- assets/js/publications.js
+++
@@ -1,9 +0,0 @@
-import { SidebarController } from "./sidebarController/SidebarController.js";
-import { Publications } from "./oop/Publications.js";
-import { dbPublications } from "./database/dbPublications.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-  const publicationList = document.querySelector('.js--publication-list');
-  new Publications(publicationList, dbPublications);
-  new SidebarController();
-});
