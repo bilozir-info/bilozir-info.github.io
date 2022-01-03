5b9751202172e4e81ef2ee168ba4343ac1252f4b
bilozir-info <96798351+bilozir-info@users.noreply.github.com>
2022-01-03 05:51:20 +0200

    Delete assets/js/assets directory

--- assets/js/assets/js/database/dbPublications.js
+++
@@ -1,8 +0,0 @@
-export const dbPublications = [
-  {
-    title: 'Розміщення публікацій',
-    text: 'Форма для подачі заявки на розміщення публікації на сайті.<br><br><a href="#/publications/new-publication">Перейти до заповнення форми</a>',
-    categories: 'site-news',
-  },
-
-]
